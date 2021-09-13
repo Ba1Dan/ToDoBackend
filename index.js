f946516ca93a471a626d7d0643b097fdc1c6a30d
Данияр <daniyar.baiganov@gmail.com>
2021-09-13 11:02:34 +0300

    Done homework

--- index.js
+++ index.js
@@ -20,22 +20,51 @@ app.use((req, res, next) => {
     next();
 })
 
-app.post("/test", function (request, response) {
+app.post("/sum", function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    response.send(request.body);
+    let num1 = request.body.num1;
+    let num2 = request.body.num2;
+    let sum = num1 + num2;
+    console.log(sum);
+    response.status(200).json({sum: sum})
 });
 
-app.get('/test', (req, res) => {
-    res.status(200).json({message: 'Good job'});
-})
-
-app.put('/test',function(request, response) {
+app.post("/reverseArray", function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    response.send(request.body);
+    let arr = request.body.array;
+    arr.reverse()
+    console.log(arr)
+    response.status(200).json({reverseArr: arr})
+});
+
+app.post("/reverseCase", function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    let str = request.body.str;
+    var result = ""
+    for (var i = 0; i < str.length; i++) {
+        result += toOppositeCase(str[i]);
+     }
+    console.log(result)
+    response.status(200).json({reverseCase: result})
+});
+
+app.get('/test', (request, response) => {
+    response.status(200).json({message: 'Good job men'});
+})
+
+app.put('/test1',function(request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.status(200).json({message: 'Ok'});
 });
 
 http.createServer(app).listen( 3000, () => {
     console.log("Server is working on port")
 })
+
+function toOppositeCase(char) {
+    return (/[a-z]/).test(char) ? char.toUpperCase() : char.toLowerCase();
+}
